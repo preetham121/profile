e8333b0f3cc5c3cd7d2f541f3d650641ccb356be
unknown <preetham.k.1211@gmail.com>
2020-05-05 16:04:27 +0530

    first commit

--- app.js
+++ app.js
@@ -16,3 +16,114 @@ function list_items2(){
     burger2.style.display = "block";
 }
 
+function information(){
+    let skills = document.querySelector(".addon");
+skills.style.transition ="1s";
+    skills.style.display = "block";
+   let mySkills = document.querySelector(".click");
+
+
+   mySkills.style.display = "none";
+
+   let dubmySkills = document.querySelector(".dubclick");
+   
+
+   dubmySkills.style.display = "block";
+
+
+
+}
+
+function dubinfo(){
+    let skills = document.querySelector(".addon");
+skills.style.transition ="1s";
+    skills.style.display = "none";
+   let mySkills = document.querySelector(".click");
+
+
+   mySkills.style.display = "block";
+
+   let dubmySkills = document.querySelector(".dubclick");
+   
+
+   dubmySkills.style.display = "none";
+
+
+
+}
+
+
+
+function information2(){
+    let skills2 = document.querySelector(".addon2");
+skills2.style.transition ="1s";
+    skills2.style.display = "block";
+   let mySkills2= document.querySelector(".click2");
+
+
+   mySkills2.style.display = "none";
+
+   let dubmySkills2 = document.querySelector(".dubclick2");
+   
+
+   dubmySkills2.style.display = "block";
+
+}
+
+function dubinfo2(){
+    let skills2 = document.querySelector(".addon2");
+skills2.style.transition ="1s";
+    skills2.style.display = "none";
+   let mySkills2 = document.querySelector(".click2");
+
+
+   mySkills2.style.display = "block";
+
+   let dubmySkills2 = document.querySelector(".dubclick2");
+   
+
+   dubmySkills2.style.display = "none";
+
+}
+
+
+function picture(){
+let pics = document.querySelector(".pics");
+pics.style.display  = "none";
+let pictures = document.getElementById("forPictures");
+pictures.style.display = "block";
+let dubpics = document.querySelector(".pics2");
+dubpics.style.display = "block";
+
+}
+
+function reversepicture(){
+    let pics = document.querySelector(".pics");
+    pics.style.display  = "block";
+    let pictures = document.getElementById("forPictures");
+    pictures.style.display = "none";
+    let dubpics = document.querySelector(".pics2");
+    dubpics.style.display = "none";
+    
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
