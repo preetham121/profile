300dfc175bfb98b7d67178ef924359f75dff236b
unknown <preetham.k.1211@gmail.com>
2020-05-05 11:32:20 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+function list_items(){
+    let list = document.querySelector(".lists");
+    list.style.display = "block";
+    let burger = document.querySelector(".burger");
+    burger.style.display = "none";
+    let burger2 = document.querySelector(".burger2");
+    burger2.style.display = "block";
+}
+
+function list_items2(){
+    let list = document.querySelector(".lists");
+    list.style.display = "none";
+    let burger = document.querySelector(".burger2");
+    burger.style.display = "none";
+    let burger2 = document.querySelector(".burger");
+    burger2.style.display = "block";
+}
+
